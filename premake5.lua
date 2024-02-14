0883f03321f8c38711659fef7860f30a2d1b6f0d
lukas <urbanlukas@protonmail.com>
2024-02-14 21:26:11 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -11,31 +11,31 @@ project "zlib"
 	objdir ("bin-obj/" .. outputdir .. "/%{prj.name}")
 
 	files {
-		"zconf.h"
-		"zlib.h"
-		"adler32.c"
-		"compress.c"
-		"crc32.c"
-		"crc32.h"
-		"deflate.c"
-		"deflate.h"
-		"gzclose.c"
-		"gzguts.h"
-		"gzlib.c"
-		"gzread.c"
-		"gzwrite.c"
-		"infback.c"
-		"inffast.c"
-		"inffast.h"
-		"inffixed.h"
-		"inflate.c"
-		"inflate.h"
-		"inftrees.c"
-		"inftrees.h"
-		"trees.c"
-		"trees.h"
-		"uncompr.c"
-		"zutil.c"
+		"zconf.h",
+		"zlib.h",
+		"adler32.c",
+		"compress.c",
+		"crc32.c",
+		"crc32.h",
+		"deflate.c",
+		"deflate.h",
+		"gzclose.c",
+		"gzguts.h",
+		"gzlib.c",
+		"gzread.c",
+		"gzwrite.c",
+		"infback.c",
+		"inffast.c",
+		"inffast.h",
+		"inffixed.h",
+		"inflate.c",
+		"inflate.h",
+		"inftrees.c",
+		"inftrees.h",
+		"trees.c",
+		"trees.h",
+		"uncompr.c",
+		"zutil.c",
 		"zutil.h"
 	}
 
